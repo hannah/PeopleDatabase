6d2251d4c28cef6d21313a91e45ca30b74271a68
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-07-31 12:57:43 -0700

    First commit

---
+++ PeopleDatabase/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PeopleDatabase
+//
+//  Created by H Zabarsky on 7/31/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
